ad396bc696922f2464563430bddce5e602b36b3b
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 21:30:53 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-def unsafe?(speed)
-if speed < 40  || speed > 60
-  return true
+# def unsafe?(speed)
+# if speed < 40  || speed > 60
+#   return true
   
-elsif speed > 40 || speed <= 60
-  false 
- end
-end 
-unsafe?(40)
+# elsif speed > 40 || speed <= 60
+#   false 
+# end
+# end 
+# unsafe?(40)
 
 
 def not_safe?(speed)
