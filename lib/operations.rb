f7e6c7f58886a7b8afba8c6c1232c5ce00abd2ae
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 21:33:23 +0000

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
 def unsafe?(speed)
-
+if speed < 40  || speed > 60
+  return true
+  
+elsif speed > 40 || speed <= 60
+  false 
 end
-
+end 
+unsafe?(40)
 
 
 def not_safe?(speed)
-	
+speed > 60 || speed < 40 ? true : false
 end
 	
 
+#Other version of unsafe using Ternary 
+# def unsafe?(speed)
+#   speed >60 ||speed < 40 ? true : false
+# end	end
 
